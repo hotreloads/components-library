febd6ae6350d2d915dffdac202453db17b61fa93
hotreloads <104692449+hotreloads@users.noreply.github.com>
2023-08-21 09:40:25 -0500

    Added more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import FaqFour from "./Components/landing/faq/FaqFour";
-import FeatureFour from "./Components/landing/feature/FeatureFour";
-import FooterOne from "./Components/landing/footer/FooterOne";
-import HeroOne from "./Components/landing/hero/HeroOne";
-import NavOne from "./Components/landing/nav/NavOne";
-import PricingThree from "./Components/landing/pricing/PricingThree";
+import HeaderOne from "./Components/app/header/HeaderOne";
 
 function App() {
 	return (
 		<main className="min-h-screen flex flex-col">
-			<NavOne />
-			<HeroOne />
-			<FeatureFour />
-			<PricingThree />
-			<FaqFour />
-			<FooterOne />
+			<HeaderOne />
 		</main>
 	);
 }
